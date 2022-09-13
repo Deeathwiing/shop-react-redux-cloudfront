e4f3ddf22d264afe25cf409258cc35fce972ce55
Deeathwiing <andreykorotkins@gmail.com>
2022-09-13 22:07:28 +0300

    Complete task 4

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,12 +1,12 @@
 const API_PATHS = {
   products:
-    "https://62zixy6hfg.execute-api.eu-central-1.amazonaws.com/dev/products",
+    "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev/products",
   product:
-    "https://62zixy6hfg.execute-api.eu-central-1.amazonaws.com/dev/product",
-  order: "https://62zixy6hfg.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://62zixy6hfg.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://62zixy6hfg.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://62zixy6hfg.execute-api.eu-central-1.amazonaws.com/dev"
+    "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev/product",
+  order: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev",
+  cart: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev"
 };
 
 export default API_PATHS;
