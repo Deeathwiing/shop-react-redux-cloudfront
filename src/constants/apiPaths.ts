0e27b20cff07cb52e6980b4469badad947351c4f
Deeathwiing <andreykorotkins@gmail.com>
2022-10-11 23:26:56 +0300

    Complete task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   product:
     "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev/product",
   order: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://kxjkwtpnmg.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://7pvpn16wji.execute-api.eu-central-1.amazonaws.com/dev"
 };
